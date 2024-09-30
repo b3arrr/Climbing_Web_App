1597bf36318ce4cabd202fe2c9715aefee09a5f6
Bjornar Aarset <32538579+b3arrr@users.noreply.github.com>
2024-09-30 23:43:34 +0200

    update routing

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import AboutPage from "./pages/AboutPage";
 import GradePage from "./pages/GradePage";
+import HomePage from "./pages/HomePage";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
     return (
         <>
             <Router>
                 <Routes>
+                    <Route path="/" element={<HomePage/>} />
+                    <Route path="/profile" element={<ProfilePage/>} />
                     <Route path="/about" element={<AboutPage />} />
                     <Route path="/grade" element={<GradePage />} />
                 </Routes>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,22 @@
+import { Outlet, Link } from "react-router-dom";
 
 function Header() {
-    return(
+    return (
         <>
             <nav>
                 <h1>Header</h1>
                 <ul>
-                    <li>Home</li>
-                    <li>Profile</li>
-                    <li>Grading</li>
-                    <li>About</li>
-                    <li>History</li>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li><Link to="/profile">Profile</Link></li>
+                    <li><Link to="/grade">Grade</Link></li>
+                    <li><Link to="/about">About</Link></li>
                 </ul>
             </nav>
+            <Outlet />
         </>
-    ) 
+    );
 }
 
 export default Header;
